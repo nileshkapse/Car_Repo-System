4a85a989793f8d4df0b781ce4012788e0f6de15f
nileshkapse <kapsenileshp18@gmail.com>
2022-09-23 23:59:56 +0530

    minor changes

--- src/pages/dashboardpages/UploadDetails.js
+++ src/pages/dashboardpages/UploadDetails.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import {useNavigate} from "react-router-dom"
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 function UploadDetails() {
-    const nevigate=useNavigate()
+  const nevigate = useNavigate();
   return (
-    <div><main id="main" className='main'>Upload Details Niles kapse </main><Navbar/><Sidebar/></div>
-  )
+    <div>
+      <main id="main" className="main">
+        <div class="file-upload">
+          <button class="file-upload-btn" type="button">
+            Add Files
+          </button>
+          <div class="image-upload-wrap">
+            <input class="file-upload-input" type="file" />
+            <div class="drag-text">
+              <h3>Drag and drop a file or select CSV</h3>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
 }
 
-export default UploadDetails
+export default UploadDetails;
